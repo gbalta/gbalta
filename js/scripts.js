1e3f9e9b9b8d3f5686cd388ca4626d1eb5238c12
gbalta <45952530+gbalta@users.noreply.github.com>
2022-06-04 16:33:31 -0400

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,11 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+function showhide(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+   }
